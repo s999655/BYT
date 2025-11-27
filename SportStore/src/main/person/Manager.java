52c9a4c75efe3a7ea19303ffff5864251f94d5f5
ccathalm <cathalmurphy3120@gmail.com>
2025-11-27 15:55:21 +0100

    Update Manager.java
    Added class extent

--- SportStore/src/main/person/Manager.java
+++ SportStore/src/main/person/Manager.java
@@ -1,12 +1,38 @@
 package main.person;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.*;
+import java.util.*;
 
-public class Manager extends Staff {
+public class Manager extends Staff implements Serializable{
 
+    private static final long serialVersionUID = 1L;
+    
+    private static List<Manager> extent = new ArrayList<>();
+
+    public static List<Manager> getExtent() {
+        return Collections.unmodifiableList(extent);
+    }
+
+    private static void addToExtent(Manager manager) {
+        if (manager == null) {
+            throw new IllegalArgumentException("Manager cannot be null");
+        }
+        extent.add(manager);
+    }
+    
     private LocalDate promotionDate;
 
+    public Manager() {
+    }
+    
     public Manager(String name, String surname, String email, String phoneNumber, int employeeNumber, LocalDate employmentDate) {
         super(name, surname, email, phoneNumber, employeeNumber, employmentDate);
+        addToExtent(this);
     }
     
     //for the optional attribute promotionDate
@@ -27,4 +53,20 @@ public class Manager extends Staff {
         this.promotionDate = promotionDate;
     }
 
+    public static void saveExtent(String fileName) {
+        try (XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(fileName)))) {
+            encoder.writeObject(extent);
+        } catch (IOException e) {
+            throw new RuntimeException("Error saving Manager extent to XML", e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void loadExtent(String fileName) {
+        try (XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream(fileName)))) {
+            extent = (List<Manager>) decoder.readObject();
+        } catch (FileNotFoundException e) {
+            extent = new ArrayList<>();
+        }
+    }
 }
